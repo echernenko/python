9daaf2ec568b674ff95639054d45a81ba9438d45
Eugene Chernenko <chernenko@gmail.com>
2020-07-07 23:06:35 +0200

    lists and string concatenation

--- py_project/pro1.py
+++ py_project/pro1.py
@@ -2,5 +2,8 @@ name1 = "Hello"
 name1 = "Hi"
 name2 = 10
 name3 = 10.5
-print(name1, name2, name3);
-print(type(name2), type(name3));
+mylist1 = [1, "Hi there", 10.5, name2]
+string1 = "h" + "1";
+print(mylist1);
+print(string1);
+# print(type(name2), type(name3));
