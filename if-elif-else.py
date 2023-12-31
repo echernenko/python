ed767f2621ff81447a6239b925ca14d7e7b849b9
Eugene Chernenko <chernenko@gmail.com>
2023-12-30 17:25:36 -0800

    Create if-elif-else.py

---
+++ if-elif-else.py
@@ -0,0 +1,13 @@
+Percentage = 82
+output = 'Grade ';
+if (Percentage >=90):
+    output += 'A'
+elif (Percentage >=80):
+    output += 'B' 
+elif (Percentage >=70):
+    output += 'C' 
+elif (Percentage >=60):
+    output += 'D' 
+else :
+    output += 'F'
+print(output)
